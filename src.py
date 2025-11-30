5dca396847f0ef5643f88857178c2f812ebfdcd3
wiillytom <138128168+wiillytom@users.noreply.github.com>
2025-11-30 18:40:39 +0100

    Integrated FRED API yields and fixed Interest_Rates class errors

--- src.py
+++ src.py
@@ -1,4 +1,84 @@
-from dataclasses import dataclass
+from dataclasses import dataclass, field
 from typing import Optional
-from datetime import datetime
+from datetime import datetime, timedelta
 import yfinance as yf
+from fredapi import Fred
+import pandas as pd
+
+fred = Fred(api_key='5079f41d061a4037d81f3da69e018803')
+
+frequency_mapping_dict = {
+    'Annually': 1,
+    'Semi-Annually': 2,
+    'Quarterly': 4
+}
+
+@dataclass 
+class Stock:
+    Ticker: str
+
+    def price(self):
+        return yf.Ticker(self.Ticker).fast_info['lastPrice']
+
+@dataclass
+class Interest_Rates:
+    Maturity: str
+    Frequency: str
+
+    Num_Frequency: int = field(init=False)
+    T: int = field(init=False)
+
+    def __post_init__(self):
+        try:
+            self.Num_Frequency = frequency_mapping_dict[self.Frequency]
+        except KeyError:
+            raise ValueError(f'Incorrect Frequency: {self.Frequency} should be one of {list(frequency_mapping_dict.keys())}')
+
+        try:
+            self.T = (datetime.strptime(self.Maturity, '%Y-%m-%d')-datetime.today()).days/365
+        except ValueError:
+            raise ValueError(f'Incorrect date : {self.Maturity}. Please use YYYY-MM-DD format')
+
+    def _most_recent_business_day():
+        ref_date = datetime.today().date()
+        while ref_date.weekday() >= 5: 
+            ref_date -= timedelta(days=1)
+        previous_bd = ref_date-timedelta(days=1)
+        data = pd.Series()
+        while len(data)==0:
+            data = fred.get_series('DGS1', observation_start= previous_bd, observation_end = last_bd)
+            previous_bd -= timedelta(days=1)
+        
+        return ref_date.isoformat(), previous_bd.isoformat()
+
+
+@dataclass
+class Convertible:
+    Face_Value: float
+    Coupon: float
+    Coupon_Frequency : str
+    Conversion_Ratio : float
+    Conversion_Strike : float 
+
+    First_Coupon_Date : str
+    Maturity: str
+    Redemption : float
+    Underlying_stock_ticker : str
+    Day_Count_Convention : Optional[str] = 'ACT/ACT'
+    Bond_Currency : Optional[str] = 'EUR'
+    Stock_Currency : Optional[str] = 'EUR'
+    AI_on_Conversion: Optional[bool] = True
+
+    def conversion_price(self):
+        return self.Face_Value/self.Conversion_Ratio
+    
+    def parity(self):
+        #Equivalent value in shares if converted right now
+        return None
+
+@dataclass
+class Option:
+    Underlying: str
+    K : float
+    T : str  
+    r = yf.Ticker('e')
